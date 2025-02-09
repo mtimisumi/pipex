da3f2bab6f4bb2bea8514cd1525481352b23292e
Mitani Misumi <mmisumi@f0r1s6.codam.nl>
2025-02-09 14:45:21 +0100

    sunday

--- getpath.c
+++ getpath.c
@@ -1,5 +1,9 @@
 #include "pipex.h"
 
+#define CHILD 0
+#define READ 0
+#define WRITE 1
+
 char	*get_env(char **argv, char **envp)
 {
 	char	*env;
@@ -11,34 +15,113 @@ char	*get_env(char **argv, char **envp)
 		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
 		{
 			env = envp[i] + 5;
+			printf("env: %s\n", env);
 			if (! env)
-				perror(1);
+				perror("1");
 			break ;
 		}
 		i++;
 	}
-	printf("envp[i]: %s\n", envp[i]);
 	return (env);
 }
 
-char	*getpath(char *path, char **cmd)
+char	**get_path(char *path, char **cmd)
 {
-
-}
-
-int	main(int argc, char *argv[], char *envp[])
-{
-	char	*path;
-	char	**cmd;
+	char	**paths;
+	char	*temp;
 	int		i;
 
 	i = 0;
-	path = get_env(argv, envp);
-	cmd = ft_split(argv[2], ' ');
-	while (cmd[i])
+	paths = ft_split(path, ':');
+	while (paths[i])
 	{
-		printf("%s\n", cmd[i]);
+		temp = ft_strjoin(paths[i], "/");
+		free(paths[i]);
+		paths[i] = ft_strjoin(temp, cmd[0]);
+		free(temp);
 		i++;
 	}
-	getpath(path, cmd);
+	return (paths);
+}
+
+char	*get_cmd(char **argv, char **envp)
+{
+	char	*path;
+	char	**cmd;
+	char	**paths;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	path = get_env(argv, envp);
+	printf("path: %s\n", path);
+	printf("argv[2]: %s\n", argv[2]);
+	cmd = ft_split(argv[2], ' ');
+	while (cmd[j])
+	{
+		printf("cmd[%d} %s\n", j, cmd[j]);
+		j++;
+	}
+	paths = get_path(path, cmd);
+	while (paths[i])
+	{
+		printf("paths[%d]: %s\n", i, paths[i]);
+		if (access(paths[i], R_OK) == 0)
+		{
+			printf("command: %s exists and is readable\n", paths[i]);
+			return (paths[i]) ;
+		}
+		else
+		{
+			free(paths[i]);
+			i++;
+		}
+	}
+	return (NULL);
+}
+
+// char	*execve_cmd()
+// {
+
+// }
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	int		fd;
+	pid_t	pid1;
+	char	*cmd;
+	int		x;
+
+	x = 0;
+	if (argc != 5)
+	{
+		printf("no goo amount of arguments");
+		return (0);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		printf("no valid file");
+		return (0);
+	}
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+	pid1 = fork();
+	if (pid1 == -1)
+	{
+		printf("forking pid1 failed");
+		return (0);
+	}
+	if (pid1 == CHILD)
+	{
+		cmd = get_cmd(argv, envp);
+		if (cmd != NULL)
+		{
+			execve(cmd, )
+		}
+	}
+	// cmd = get_cmd(argv, envp);
+	// printf("cmd: %s\n", cmd);
+	return (0);
 }
--- pipe.c
+++ pipe.c
@@ -1,66 +1,66 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   pipe.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mmisumi <mmisumi@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/12 14:34:47 by mmisumi           #+#    #+#             */
-/*   Updated: 2025/01/27 22:32:32 by mmisumi          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   pipe.c                                             :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: mmisumi <mmisumi@student.42.fr>            +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2025/01/12 14:34:47 by mmisumi           #+#    #+#             */
+// /*   Updated: 2025/01/27 22:32:32 by mmisumi          ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
 
-#include "pipex.h"
+// #include "pipex.h"
 
-int	main(int argc, char *argv[])
-{
-	int	fd[2];//fd[0] = read end, fd[1] = write end
-	int	id;
-	int	x;
-	int	y;
+// int	main(int argc, char *argv[])
+// {
+// 	int	fd[2];//fd[0] = read end, fd[1] = write end
+// 	int	id;
+// 	int	x;
+// 	int	y;
 
-	if (pipe(fd))//creates the pipe and has the two "keys" to that pipe
-	{
-		printf("An error occured with opening the pipe\n");
-		return (-1);
-	}
-	id = fork();
-	if (id == -1)
-		return (-1);
-	if (id == 0)//child process
-	{
-		close (fd[0]);
-		printf("Input a number: ");
-		scanf("%d", &x);
-		if (write(fd[1], &x, sizeof(int)) == -1)
-			return (-1);
-		close (fd[1]);
-	}
-	else//parent process
-	{
-		close (fd[1]);
-		if (read(fd[0], &y, sizeof(int)) == -1)
-			return (-1);
-		close (fd[0]);
-		printf("Got from childprocess: %d\n", y);
-	}
-	return (0);
-}
+// 	if (pipe(fd))//creates the pipe and has the two "keys" to that pipe
+// 	{
+// 		printf("An error occured with opening the pipe\n");
+// 		return (-1);
+// 	}
+// 	id = fork();
+// 	if (id == -1)
+// 		return (-1);
+// 	if (id == 0)//child process
+// 	{
+// 		close (fd[0]);
+// 		printf("Input a number: ");
+// 		scanf("%d", &x);
+// 		if (write(fd[1], &x, sizeof(int)) == -1)
+// 			return (-1);
+// 		close (fd[1]);
+// 	}
+// 	else//parent process
+// 	{
+// 		close (fd[1]);
+// 		if (read(fd[0], &y, sizeof(int)) == -1)
+// 			return (-1);
+// 		close (fd[0]);
+// 		printf("Got from childprocess: %d\n", y);
+// 	}
+// 	return (0);
+// }
 
-int	main(int argc, char *argv[])
-{
-	int	fd[2];
-	int	pid1;
+// int	main(int argc, char *argv[])
+// {
+// 	int	fd[2];
+// 	int	pid1;
 
-	if (pipe(fd) == -1)
-		return (-1);
-	pid1 = fork();
-	if (pid1 == -1)
-		return (-1);
-	if (pid1 == 0)//in child process
-	{
-		execlp;//we should use execve instead to get to commands via path
-	}
-	waitpid(pid1, NULL, 0);
-	return (0);
-}
+// 	if (pipe(fd) == -1)
+// 		return (-1);
+// 	pid1 = fork();
+// 	if (pid1 == -1)
+// 		return (-1);
+// 	if (pid1 == 0)//in child process
+// 	{
+// 		execlp;//we should use execve instead to get to commands via path
+// 	}
+// 	waitpid(pid1, NULL, 0);
+// 	return (0);
+// }
